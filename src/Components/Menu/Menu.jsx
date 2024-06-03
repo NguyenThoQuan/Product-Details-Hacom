3d75ef9329e3159fcfe667996bc296504d04e2f0
NguyenThoQuan <thoquan150903@gmail.com>
2024-06-03 16:52:18 +0700

    Final UI

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Searchh from "./Search";
 import Personal from "./Personal";
 import "../../Styles/Menu.scss";
 import logo from "../../Images/logo-hacom-compressed4.f1d79573.svg"
 
 export default function Menu() {
+    const [isSticky, setIsSticky] = useState(false);
+
+    const handleScroll = () => {
+        if (window.scrollY > 50) {
+            setIsSticky(true);
+        } else {
+            setIsSticky(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <div className="menu">
+        <div className={isSticky === true ? "sticky" : "menu"}>
             <img src={logo} alt="logo" />
             <Searchh />
             <Personal />
