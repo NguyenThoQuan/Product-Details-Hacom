76c5550ed8f39bb234fa7c786a6071d1e7f8db93
NguyenThoQuan <thoquan150903@gmail.com>
2024-06-03 23:31:27 +0700

    UpdateHover

--- src/Components/SimilarProduct/HoverSimilar.jsx
+++ src/Components/SimilarProduct/HoverSimilar.jsx
@@ -1,15 +1,30 @@
 import React from "react";
+import { Stack } from "react-bootstrap-icons";
 
-export default function HoverSimilar({ selectedAppointment }) {
+export default function HoverSimilar({ product }) {
     return (
-        <div className="hovet-similar">
-            {
-                selectedAppointment.map((item, index) => {
-                    <div key={index}>
-                        {item.name}
-                    </div>
-                })
-            }
+        <div className="hover-similar">
+            <div className="hover-similar-content">
+                <span className="name">{product.name}</span>
+                <div className="hover-details">
+                    <span className="span-1">- Giá bán:</span><span> {product.promotion}</span><br />
+                    <span className="span-3">- Giá HACOM:</span><span className="span-4"> {product.cost}</span><span className="vat">[Đã bao gồm VAT]</span><br />
+                    <span className="span-5">- Bảo hành:</span><span> 12 tháng - BH 3S1</span><br />
+                </div>
+                <div className="title">
+                    <span><Stack /> Thông số sản phẩm</span>
+                </div>
+                <div className="info-product">
+                    <p>- CPU: AMD R5 5625U</p>
+                    <p>- RAM: 8GB</p>
+                    <p>- Ổ cứng: 512GB SSD</p>
+                    <p>- VGA: NVIDIA RTX3050 4GB</p>
+                    <p>- Màn hình: 15.6 inch FHD 144Hz</p>
+                    <p>- HĐH: Window 11</p>
+                    <p>- CPU: AMD R5 5625U</p>
+                    <p>- Màu: Đen</p>
+                </div>
+            </div>
         </div>
     )
 }
--- src/Components/SimilarProduct/SimilarProduct.jsx
+++ src/Components/SimilarProduct/SimilarProduct.jsx
@@ -4,7 +4,7 @@ import { Calendar3Event, StarFill, Check, Handbag } from "react-bootstrap-icons"
 import "../../Styles/SimilarProduct.scss";
 
 export default function SimilarProduct() {
-    const [similarProduct, setSimilarProduct] = useState([
+    const [similarProduct] = useState([
         { percent: "-7%", images: "/images/SP1.jpg", name: "Laptop Acer Gaming Aspire 7 A715-42G-R4XX (NH.QAYSV.008) (R5 5500U/8GB RAM/256GB SSD/15.6 inch FHD/GTX1650 4G/Win11/Đen)", promotion: "16.650.000 đ", cost: "15.499.000 đ" },
         { percent: "-33%", images: "/images/SP2.png", name: "Laptop Acer Gaming Aspire 7 A715-43G-R8GA (NH.QHDSV.002) (R5 5625U/8GB RAM/512GB SSD/15.6 inch FHD 144Hz/RTX3050 4G/Win11/Đen) ", promotion: "23.999.000 đ", cost: "15.999.000 đ" },
         { percent: "-9%", images: "/images/SP3.jpg", name: "Laptop Acer Gaming Aspire 7 A715-76G-5806 (NH.QMFSV.002) (i5 12450H/16GB RAM/512GB SSD/RTX 3050 4G/15.6 inch FHD 144Hz/Win11/Đen/Vỏ nhôm) ", promotion: "22.999.000 đ", cost: "20.999.000 đ" },
@@ -13,17 +13,10 @@ export default function SimilarProduct() {
 
     const [clickOption, setClickOption] = useState(false);
 
-    const [onHover, setOnHover] = useState(false);
-
-    const [selectedAppointment, setSelectedAppointment] = useState([]);
+    const [hoveredProduct, setHoveredProduct] = useState(null);
 
     const handleClickOption = () => {
         setClickOption(!clickOption);
-    }
-
-    const handleHover = (appointment) => {
-        setSelectedAppointment(appointment);
-        setOnHover(!onHover);
     }
 
     return (
@@ -35,7 +28,7 @@ export default function SimilarProduct() {
             <div className="details-SM">
                 {
                     similarProduct && similarProduct.map((item, index) => (
-                        <div key={index} className="SM">
+                        <div key={index} className="SM" onMouseEnter={() => setHoveredProduct(item)} onMouseLeave={() => setHoveredProduct(null)}>
                             <div className="percent">
                                 <div>{item.percent}</div>
                             </div>
@@ -55,9 +48,7 @@ export default function SimilarProduct() {
                                     </div>
                                 </div>
                             </div>
-                            {
-                                onHover ? <HoverSimilar /> : <></>
-                            }
+                            {hoveredProduct === item && <HoverSimilar product={item} />}
                         </div>
                     ))
                 }
