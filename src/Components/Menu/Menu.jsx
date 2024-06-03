04c44927fae8781f7006ee424b9a7673c8bcfb90
NguyenThoQuan <thoquan150903@gmail.com>
2024-06-03 17:07:51 +0700

    NavScroll

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Searchh from "./Search";
 import Personal from "./Personal";
+import { List } from "react-bootstrap-icons";
 import "../../Styles/Menu.scss";
 import logo from "../../Images/logo-hacom-compressed4.f1d79573.svg"
 
@@ -23,10 +24,14 @@ export default function Menu() {
     }, []);
 
     return (
-        <div className={isSticky === true ? "sticky" : "menu"}>
+        <div className={isSticky ? "sticky" : "menu"}>
+            {
+                isSticky ? <div className="new-menu"><List className="icon" />Danh mục</div> : <></>
+            }
             <img src={logo} alt="logo" />
             <Searchh />
             <Personal />
+            
         </div>
     )
 }
